2aba6f12e56561dcb575663113e00b5ad588fbcd
MajidAbdolshah <majid@deakin.edu.au>
2018-10-15 19:39:57 +1100

    Trying to optimize

--- Codes/Second_Paper/RankedBO.py
+++ Codes/Second_Paper/RankedBO.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from scipy.stats import norm
 from copy import deepcopy
 import GPy
 from scipy.stats import mvn
@@ -168,6 +169,7 @@ def samplePareto(par):
 
     cells_ = np.empty((0,OUTPUT_DIM*2))
     Jparet_ = np.empty((0,OUTPUT_DIM*2))
+    not_Jparet_ = np.empty((0,OUTPUT_DIM*2))
     for i in range(0,len(uMapX)-1):
         for j in range(0,len(uMapY)-1):
 
@@ -181,8 +183,11 @@ def samplePareto(par):
             infoDic[repr(pos_)] = ruPareto(mid_point,par)
             if ruPareto(mid_point,par):
                 Jparet_ = np.vstack([Jparet_,pos_])
+            else:
+                not_Jparet_ = np.vstack([not_Jparet_,pos_])
 
-    return cells_,Jparet_,infoDic
+
+    return cells_,Jparet_,not_Jparet_,infoDic
 
 
 def ruPareto(x,par):
@@ -191,30 +196,30 @@ def ruPareto(x,par):
             return False
     return True
 
-def dvt_mu(xs,Data,Kernels,yReal,Ker_):
-    len_matrix = -np.linalg.pinv(np.identity(INPUT_DIM)*LEN_SCALE**2)
+def dvt_mu(xs,Data,Kernels,yReal,Ker_,Kinv):
+    #len_matrix = -np.linalg.inv(np.identity(INPUT_DIM)*LEN_SCALE**2)
+    len_matrix = -(np.identity(INPUT_DIM)*(1/(LEN_SCALE**2)))
     XsT = (Data - xs).T
     tmpI = np.dot(len_matrix,XsT)
     KxsX = Kernels[Ker_].K(xs,Data).T
-    tmpII = np.dot(np.linalg.pinv(Kernels[Ker_].K(Data,Data)),np.matrix(yReal).T)
+    #tmpII = np.dot(np.linalg.pinv(Kernels[Ker_].K(Data,Data)),np.matrix(yReal).T)
+    tmpII = np.dot(Kinv,np.matrix(yReal).T)
     tmpIII = np.multiply(KxsX,tmpII)
     res_ = np.dot(tmpI,tmpIII)
     return res_
 
-def dvt_var(xs,Data,Kernels,Ker_):
+def dvt_var(xs,Data,Kernels,Ker_,Kinv):
     sizeD = Data.shape[0]
     sizeX = xs.shape[1]
-    len_matrix = np.linalg.inv(np.identity(INPUT_DIM)*LEN_SCALE**2)
-    KXX_m1 = np.linalg.pinv(Kernels[Ker_].K(Data,Data))
+    len_matrix = (np.identity(INPUT_DIM)*(1/(LEN_SCALE**2)))
+    KXX_m1 = Kinv#np.linalg.pinv(Kernels[Ker_].K(Data,Data))
     KxsX = Kernels[Ker_].K(xs,Data)
     KXxs = Kernels[Ker_].K(Data,xs)
     X_xs = (Data-xs).T
-
     sumup_ = np.zeros([sizeX,sizeX])
     Alis_ = 0
     for i in range(0,sizeD):
         for j in range(0,sizeD):
-
             tmpI = np.dot(np.dot(np.matrix(X_xs[:,i]).T,np.matrix(X_xs[:,j])),len_matrix**2)
             tmp1 = KXX_m1[i,j]*KxsX[0,i]*KxsX[0,j]
             Alis_ += tmp1*tmpI
@@ -222,7 +227,7 @@ def dvt_var(xs,Data,Kernels,Ker_):
     return (len_matrix + Alis_)
 
 def Helpme_(points):
-    grid_,J,dic = samplePareto(points)
+    grid_,J,NJ,dic = samplePareto(points)
     for val in grid_:
         if np.invert(dic[repr(val)]):
             plt.plot(val[0,2],val[0,3],'*r',markersize=15)
@@ -237,22 +242,21 @@ def cell_point_dom(points,cell):
     return res_
 
 def Expected_HVI(points,weights):
-    grid_,J,pMap_ = samplePareto(points)
+    grid_,Jgrid_,Not_Jgrid_,pMap_ = samplePareto(points)
     w_area = 0
     j_area = 0
     wPoints = {}
     Weightsdic = {}
-    for val in grid_:
-        if np.invert(pMap_[repr(val)]):
-            wPoints[repr(val)] = cell_point_dom(points,val)
-            if (len(wPoints[repr(val)])):
-                exWeights = 1 - np.prod(np.take(1-weights,wPoints[repr(val)]))
-            else:
-                exWeights = 1
-            area_ = (val[0,3] - val[0,1])*(val[0,2] - val[0,0])
-            w_area += exWeights*area_
-            j_area += area_
-            Weightsdic[repr(val)] = exWeights
+    for val in Not_Jgrid_:
+        wPoints[repr(val)] = cell_point_dom(points,val)
+        if (len(wPoints[repr(val)])):
+            exWeights = 1 - np.prod(np.take(1-weights,wPoints[repr(val)]))
+        else:
+            exWeights = 1
+        area_ = (val[0,3] - val[0,1])*(val[0,2] - val[0,0])
+        w_area += exWeights*area_
+        j_area += area_
+        Weightsdic[repr(val)] = exWeights
     return w_area
 
 def createPoints(deepness,breadth):
@@ -312,7 +316,7 @@ def findIntegral(Mu,Sigma):
     return sumProb
 
 def plot_me(yPareto,Probs):
-    grid_,J,dic = samplePareto(yPareto)
+    grid_,J,NJ,dic = samplePareto(yPareto)
     for val in grid_:
         '''
         if dic[repr(val)]:
@@ -325,13 +329,13 @@ def plot_me(yPareto,Probs):
         plt.text(yPareto[i,0]-0.5,yPareto[i,1]-0.5,np.round(Probs[i],5),size=14)
     plt.show()
 
-def Derivatives(x,xPareto,dataset,Kernels):
+def Derivatives(x,xPareto,dataset,Kernels,Kinv_0,Kinv_1):
     Mu = {}
     Sigma = {}
-    Mu[0] = (dvt_mu(x,dataset.data,Kernels,dataset.outputs[:,0],'ker0'))
-    Mu[1] = (dvt_mu(x,dataset.data,Kernels,dataset.outputs[:,1],'ker1'))
-    Sigma[0] = (dvt_var(x,dataset.data,Kernels,'ker0'))
-    Sigma[1] = (dvt_var(x,dataset.data,Kernels,'ker1'))
+    Mu[0] = (dvt_mu(x,dataset.data,Kernels,dataset.outputs[:,0],'ker0',Kinv_0))
+    Mu[1] = (dvt_mu(x,dataset.data,Kernels,dataset.outputs[:,1],'ker1',Kinv_1))
+    Sigma[0] = (dvt_var(x,dataset.data,Kernels,'ker0',Kinv_0))
+    Sigma[1] = (dvt_var(x,dataset.data,Kernels,'ker1',Kinv_1))
     return Mu,Sigma
 
 def sampleXs(X,Y,bound):
@@ -346,18 +350,23 @@ def sampleXs(X,Y,bound):
     finalInd = list(set(fIndx_0) & set(fIndx_1))
     return X.T[finalInd],Y.T[finalInd]
 
-def WeightPoints(xPareto,dataset,Kernels):
+def WeightPoints(xPareto,dataset,Kernels,Kinv_0,Kinv_1):
     Wlog = []
     RPareto = []
     for val in xPareto:
         x = np.array([val])
-        Mu,Sigma = Derivatives(x,xPareto,dataset,Kernels)
+        Mu,Sigma = Derivatives(x,xPareto,dataset,Kernels,Kinv_0,Kinv_1)
         tmp_ = findWeight(Mu,Sigma,DEEP,BREAD)
         Wlog.append(tmp_)
         RPareto.append([function(x)[0,0],function(x)[0,1]])
     Weights = np.array(Wlog)
     RPareto = np.array(RPareto)
     return Weights,RPareto
+
+def Generate_bounded(x1,x2,y1,y2,num):
+    first_ = np.random.uniform(low = x1, high = x2, size=(num,))
+    second_ = np.random.uniform(low = y1, high = y2, size=(num,))
+    return np.vstack((first_,second_)).T
 
 def AQFunc(bounds,Weights,RPareto):
     print("(***)")
@@ -370,6 +379,8 @@ if __name__ == '__main__':
     COUNTER = 3
     DEEP = 100
     BREAD = 100
+    COUNTER = 10
+    ctn = 0
     MAXSAMPLE = 10000
     bounds = dict()
     bounds  = {'min': [-5],'max':[5]}
@@ -380,61 +391,85 @@ if __name__ == '__main__':
     dataset = DataComplex(xData,yData)
     yPareto = mPareto(dataset.outputs)
     xPareto = findXpareto(dataset.data,dataset.outputs,yPareto)
-    print("_____________________________")
-    print("Data Initialized in %s seconds; OK!\n" % round(time.time() - start_time,5))
+    initial_pareto = yPareto
     print("_____________ INFO ________________")
     info(dataset.data,dataset.outputs,yPareto,xPareto)
-
-    ################# TRAIN THE MODEL
-    Kernels = ctKernel(OUTPUT_DIM,INPUT_DIM,LEN_SCALE,LEN_SCALE)
-    mod1,Kernels['ker0'] = trainModel(dataset.data,np.matrix(dataset.outputs[:,0]).T,'ker0',40)
-    mod2,Kernels['ker1'] = trainModel(dataset.data,np.matrix(dataset.outputs[:,1]).T,'ker1',40)
     X,Y = initvals_(bounds,MAXSAMPLE)
     X,Y = X.T,Y.T
-
-    xx,yy = sampleXs(X,Y,[0,0,1,3])
+    Kernels = ctKernel(OUTPUT_DIM,INPUT_DIM,LEN_SCALE,LEN_SCALE)
     print("_____________________________")
-    print("GP trained in %s seconds; OK!\n" % round(time.time() - start_time,5))
-
-    #################  FIND THE WEIGHTS AND EHVI
-    start_time = time.time()
-    Weights_,Paretos_ = WeightPoints(xPareto,dataset,Kernels)
-    EHVI = Expected_HVI(Paretos_,Weights_)
-    print("_____________________________")
-    print("Hypervolume found in %s seconds; OK!\n" % round(time.time() - start_time,5))
-    print(EHVI)
-    #################  READY TO LUNCH THE LOOP
-
-    tempoX = []
-    tempoY = []
-    copyPareto = deepcopy(yPareto)
-    copyxPareto = deepcopy(xPareto)
+    print("Data Initialized in %s seconds; OK!\n" % round(time.time() - start_time,5))
 
 
+    while ctn < COUNTER:
+        ################# TRAIN THE MODEL
+        mod1,Kernels['ker0'] = trainModel(dataset.data,np.matrix(dataset.outputs[:,0]).T,'ker0',40)
+        mod2,Kernels['ker1'] = trainModel(dataset.data,np.matrix(dataset.outputs[:,1]).T,'ker1',40)
+        Kinv_0 = np.linalg.pinv(Kernels['ker0'].K(dataset.data,dataset.data))
+        Kinv_1 = np.linalg.pinv(Kernels['ker1'].K(dataset.data,dataset.data))
+        print("_____________________________")
+        print("GP trained in %s seconds; OK!\n" % round(time.time() - start_time,5))
 
-    grid_,Jgrid_,pMap_ = samplePareto(yPareto)
-    for valg in Jgrid_:
-        xBatch,yBatch = sampleXs(X,Y,[valg[0,0],valg[0,1],valg[0,2],valg[0,3]])
-        tempoX.extend(xBatch[:,0])
-        tempoY.extend(yBatch)
-    print((tempoX[0]))
-    print((tempoY[0]))
+        #################  FIND THE WEIGHTS AND EHVI
+        start_time = time.time()
+        yPareto = mPareto(dataset.outputs)
+        xPareto = findXpareto(dataset.data,dataset.outputs,yPareto)
+        print("==============PARETO SHAPE================")
+        print(yPareto.shape)
+        print("==========================================")
+        Weights_,Paretos_ = WeightPoints(xPareto,dataset,Kernels,Kinv_0,Kinv_1)
+        EHVI = Expected_HVI(Paretos_,Weights_)
+        print("_____________________________ %s " % EHVI)
+        print("Hypervolume found in %s seconds; OK!\n" % round(time.time() - start_time,3))
+
+        #################  READY TO LUNCH THE LOOP
+        copyPareto = deepcopy(yPareto)
+        copyxPareto = deepcopy(xPareto)
+        SlidingY = np.empty(shape = (0,OUTPUT_DIM))
+        grid_,Jgrid_,Ngridholder_,pMap_ = samplePareto(yPareto)
+        for valg in Jgrid_:
+            yBatch = Generate_bounded(valg[0,0],valg[0,2],valg[0,1],valg[0,3],5)
+            SlidingY = np.vstack((SlidingY,yBatch))
+
+        print("Yshape: ",SlidingY.shape)
+        x_log = []
+        imp_log = []
+        for i in tqdm(range(len(X.T[:1000,:]))):
+            x_log.append([X.T[i,:]])
+            Total_HVI_diff = 0
+            y1,Sigy1 = testModel(mod1,np.array([X.T[i,:]]))
+            y2,Sigy2 = testModel(mod2,np.array([X.T[i,:]]))
+            temp_x_pareto = np.vstack((copyxPareto,np.array([X.T[i,:]])))
+            New_Weights_,New_Paretos_ = WeightPoints(temp_x_pareto,dataset,Kernels,Kinv_0,Kinv_1)
+
+            slide_size = len(SlidingY)
+            for j in (range(slide_size)):
+                temp_pareto = np.vstack((copyPareto,np.array([SlidingY[j,0],SlidingY[j,1]])))
+                found_temp_pareto = mPareto(temp_pareto)
+                usef_weights = New_Weights_[parY_X(temp_pareto,found_temp_pareto)]
+
+                Probs_dim1 = norm(y1, Sigy1).pdf(SlidingY[j,0])
+                Probs_dim2 = norm(y2, Sigy2).pdf(SlidingY[j,1])
+                #EHVI_New = Expected_HVI(found_temp_pareto,usef_weights)*Probs_dim1*Probs_dim2
+                #Total_HVI_diff += EHVI_New
+            imp_log.append(Total_HVI_diff)
+        indx = imp_log.index(max(imp_log))
+        Best_x = x_log[indx][0]
+        Best_y = function(np.array([Best_x]))[0,0],function(np.array([Best_x]))[0,1]
+        dataset.newData(Best_x)
+        dataset.newOut(Best_y)
+        ctn += 1
 
 
-    for valx in X.T[:3,:]:
-        y1,Sigy1 = testModel(mod1,np.array([valx]))
-        y2,Sigy2 = testModel(mod2,np.array([valx]))
-            for randx in X.T[5:10,:]:
-                temp_pareto = np.vstack((copyPareto,np.array([y1,y2])))
-                temp_x_pareto = np.vstack((copyxPareto,np.array([valx])))
-                if np.array_equal(np.round(mPareto(temp_pareto),5),np.round(yPareto,5)):
-                    pass
-                else:
-                    print("Not the same, initiate the process!~!")
-                    New_Weights_,New_Paretos_ = WeightPoints(temp_x_pareto,dataset,Kernels)
-                    Probs_ = scipy.stats.norm(y1, 1).pdf(1)
-                    EHVI_New = Expected_HVI(New_Paretos_,New_Weights_)
-                    print(EHVI_New - EHVI)
+    plt.plot(initial_pareto[:,0],initial_pareto[:,1],"*r",markersize = 15)
+    plt.plot(yPareto[:,0],yPareto[:,1],"*b",markersize = 10)
+    plt.show()
+
+
+
+
+
+
 
 
 
@@ -443,6 +478,13 @@ if __name__ == '__main__':
 
 
     '''
+            print("==============================")
+            print("size of X is: ",dataset.data.shape)
+            print("size of Y is: ",dataset.outputs.shape)
+            print("==============================")
+    if np.array_equal(np.round(mPareto(temp_pareto),5),np.round(yPareto,5)):
+        pass
+    else:
     plot_me(Paretos_,Weights_)
     ############################################################# CALLING DERIVATIVES
     Wlog = []
@@ -499,4 +541,20 @@ if __name__ == '__main__':
             #print(Y.shape)
 
 sampleGenerator(X,mod1,mod2)
+    '''
+            #print("found_temp_pareto ",found_temp_pareto.shape)
+            #temp_x_pareto = list(map(temp_x_pareto.__getitem__, parY_X(temp_pareto,found_temp_pareto)))
+    '''
+            plt.plot(copyPareto[:,0],copyPareto[:,1],'ob',markersize=20)
+            print(valy[0],valy[1])
+            plt.plot(valy[0],valy[1],'*r',markersize=20)
+            plt.plot(found_temp_pareto[:,0],found_temp_pareto[:,1],'og')
+
+            plt.axis((-1,10,-1,10))
+            plt.show()newOut
+
+            #print("Not the same, initiate the process!~!")
+
+            print("-------------")
+            #print(New_Weights_)
     '''
